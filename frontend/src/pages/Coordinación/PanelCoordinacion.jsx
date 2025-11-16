20e67e6b535332706c304fe9dd3cdcbb4e72a203
YobiObi <gosovia.sans@gmail.com>
2025-11-16 02:13:57 -0300

    aun diseño de botones

--- frontend/src/pages/Coordinación/PanelCoordinacion.jsx
+++ frontend/src/pages/Coordinación/PanelCoordinacion.jsx
@@ -264,7 +264,7 @@ ${motivo}
                           </span>
                         </td>
 
-                        <td>
+                        <td className="col-acciones">
                           <div className="acciones-cita d-flex gap-2 flex-wrap">
                             {/* Ver detalle en modal */}
                             <button
